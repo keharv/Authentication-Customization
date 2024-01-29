bb3ebad14a2df27520b463648a073c4b8ca557d1
Internet Jesus <egod@google.com>
2024-01-28 21:03:31 -0500

    ABP Azure B2C Authentication Example

---
+++ src/Acme.BookStore.DbMigrator/DbMigratorHostedService.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Acme.BookStore.Data;
+using Serilog;
+using Volo.Abp;
+using Volo.Abp.Data;
+
+namespace Acme.BookStore.DbMigrator
+{
+    public class DbMigratorHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+        public DbMigratorHostedService(IHostApplicationLifetime hostApplicationLifetime)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var application = AbpApplicationFactory.Create<BookStoreDbMigratorModule>(options =>
+            {
+                options.UseAutofac();
+                options.Services.AddLogging(c => c.AddSerilog());
+                options.Services.AddDataMigrationEnvironment();
+            }))
+            {
+                application.Initialize();
+
+                await application
+                    .ServiceProvider
+                    .GetRequiredService<BookStoreDbMigrationService>()
+                    .MigrateAsync();
+
+                application.Shutdown();
+                
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
